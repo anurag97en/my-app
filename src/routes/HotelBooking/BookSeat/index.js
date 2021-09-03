26658c6a928c8ca4c773ac26bf6f918dd9d729d8
karnsubhash <subhashkarn25@gmail.com>
2021-09-03 22:17:49 +0530

    Test Push

--- src/routes/HotelBooking/BookSeat/index.js
+++ src/routes/HotelBooking/BookSeat/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { PopOver } from "../../../components/ReusableComponents/PopOver";
+import Chair from "../../../assets/svg/chair.svg"
 const BookSeat = () => {
   return (
     <div >
     This is my book seat component
+    <img
+        height="130px"
+        width="130px"
+        src={Chair}
+        alt="butterChicken"
+        style={{ marginTop: "10px" }}
+      />
+    {/* {} */}
+    {/* <Chair/> */}
     {/* <PopOver/> */}
     </div>
   );
